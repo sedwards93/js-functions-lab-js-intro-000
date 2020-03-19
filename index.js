4408f56b66f5b171f61a4990283d0f315bbfd97e
Symion Edwards <symion.edwards@gmail.com>
2020-03-19 18:58:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays (string) {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
-  return `Happy holidays ${name}!`
+  return `Happy holidays, ${name}!`
 }
 function happyCustomHolidayTo(holiday,name) {
   return `Happy ${holiday}, ${name}!`
